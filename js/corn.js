4b8e8ab6e0758bac7db5e64fd6586c2760faebb9
MohamedELmasry1997 <125711560+MohamedELmasry1997@users.noreply.github.com>
2025-08-02 03:34:14 +0300

    Add files via upload

---
+++ js/corn.js
@@ -0,0 +1,49 @@
+
+let myArray = [];
+
+let searchInput = document.getElementById("searchInput");
+
+let myBtn = document.querySelector(".btn-warning");
+
+let loading = document.querySelector('.loading')
+
+getRecipes();
+
+
+
+
+async function getRecipes() {
+    try {
+        loading.classList.remove("d-none");
+
+        let response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=corn`);
+        let data = await response.json()
+        myArray = data.recipes;
+        console.log(myArray);
+        loading.classList.add("d-none");
+
+        displayData();
+    } catch (error) {
+        loading.classList.add("d-none");
+        
+    }
+}
+
+getRecipes();
+function displayData() {
+    let box = ''
+    for (let i = 0; i < myArray.length; i++){
+        box += `
+        <div class="col-md-3 ">
+                            <div class="card text-center my-5 pb-5">
+                                <div class="card-img rounded ">
+                                    <img src="${myArray[i].image_url}" class="w-100" alt="">
+                                    <h4>${myArray[i].title.split(' ').splice(0,3).join(' ')}.</h4>
+                                    <a class="py-5" href="${myArray[i].source_url}">${myArray[i].source_url.split('').splice(0,20).join('')}</a>
+                                </div>
+                            </div>
+                        </div>
+        `;
+    }
+    document.getElementById('rowData').innerHTML = box
+}
---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+let myArray = [];
+
+let searchInput = document.getElementById("searchInput");
+
+let myBtn = document.querySelector(".btn-warning");
+
+let loading = document.querySelector('.loading')
+
+getRecipes("pizza");
+
+if (searchInput.value = '') {
+    getRecipes("pizza");
+}
+
+myBtn.addEventListener('click', function () {
+    getRecipes(searchInput.value)
+})
+
+async function getRecipes(meal) {
+    try {
+        loading.classList.remove("d-none");
+
+        let response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${meal}`);
+        let data = await response.json()
+        myArray = data.recipes;
+        console.log(myArray);
+        loading.classList.add("d-none");
+
+        displayData();
+    } catch (error) {
+        loading.classList.add("d-none");
+        
+    }
+}
+
+getRecipes();
+function displayData() {
+    let box = ''
+    for (let i = 0; i < myArray.length; i++){
+        box += `
+        <div class="col-sm-12 col-md-6 col-lg-4 ">
+                            <div class="card text-center my-5 pb-5">
+                                <div class="card-img rounded ">
+                                    <img src="${myArray[i].image_url}" class="w-100" alt="">
+                                    <h4>${myArray[i].title.split(' ').splice(0,3).join(' ')}.</h4>
+                                    <a class="py-5" href="${myArray[i].source_url}">${myArray[i].source_url.split('').splice(0,55).join('')}</a>
+                                </div>
+                            </div>
+                        </div>
+        `;
+    }
+    document.getElementById('rowData').innerHTML = box
+}
